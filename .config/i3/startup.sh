90d8166ffbf8cb6b6467c7ddc85bd84a90607d31
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2017-09-16 19:35:12 +0200

    Command escaping in i3 sucks

---
+++ .config/i3/startup.sh
@@ -0,0 +1,9 @@
+# Swap ESC and Caps Lock
+/usr/bin/setxkbmap -option "caps:swapescape"
+                                                                        
+# Setup keyboard layouts and switching
+/usr/bin/setxkbmap -layout us,ara
+/usr/bin/setxkbmap -option 'grp:win_space_toggle'
+                                                                        
+# Set background image
+feh --bg-scale /usr/share/archlinux/wallpaper/archlinux-simplyblack.png
