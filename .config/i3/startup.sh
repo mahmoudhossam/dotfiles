5eb50b5f28a95d45e99d2fc99af20d85b99232dd
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2018-03-20 08:14:01 +0200

    Add laptop specific configuration

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -6,7 +6,9 @@
 /usr/bin/setxkbmap -option 'grp:win_space_toggle'
                                                                         
 # Set background image
-feh --bg-scale /usr/share/archlinux/wallpaper/archlinux-simplyblack.png
+/usr/bin/feh --bg-scale /usr/share/archlinux/wallpaper/archlinux-simplyblack.png
 
 # Start gnome polkit authentication agent
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+
+/usr/lib/fingerprint-gui/fingerprint-polkit-agent -d
