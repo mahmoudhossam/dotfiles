5a630b2ecaba699ab7a147c2a731748f8bbfcec6
Mahmoud Hanafy <mahmoud.hanafy@retresco.de>
2020-10-22 21:20:42 +0200

    Swap capslock and escape in udev instead of X

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,14 +1,12 @@
-# Swap ESC and Caps Lock
-/usr/bin/setxkbmap -option "caps:swapescape"
-                                                                        
 # Setup keyboard layouts and switching
 /usr/bin/setxkbmap -layout us,ara
 /usr/bin/setxkbmap -option 'grp:win_space_toggle'
+
+# Enable Num Lock
+/usr/bin/numlockx on
                                                                         
 # Set background image
-/usr/bin/feh --bg-scale /usr/share/archlinux/wallpaper/archlinux-simplyblack.png
+/usr/bin/feh --bg-scale /usr/share/backgrounds/archlinux/archlinux-simplyblack.png
 
 # Start gnome polkit authentication agent
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-
-/usr/lib/fingerprint-gui/fingerprint-polkit-agent -d
